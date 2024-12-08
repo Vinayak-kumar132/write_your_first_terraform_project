af830a2c17f97c51923288213296ade3d5c9c0dd
Vinayak kumar <128391172+Vinayak-kumar132@users.noreply.github.com>
2024-12-08 17:57:57 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,24 +1,40 @@
 terraform {
   required_providers {
-    aws = {
+    aws = {                               // if we want an another cloud provider then we must re initialize it.
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"            //version of terraform required
 }
 
-provider "aws" {
+provider "aws" {                            // if you dont use it is fine ..not a mandatory block
   region  = "us-west-2"
 }
 
+// the above things which is written till now is constant.
+
+// if we have to create two resources we have to create 2 resource block
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform_Demo"
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
   }
 }
 
